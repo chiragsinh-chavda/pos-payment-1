651b232c9a0dc5cba1a91cda3407d78f573061cd
chiragsinh-chavda <chiragsinh.chavda@techivies.com>
2024-09-04 12:51:38 +0530

    changes

---
+++ src/copy_redirect.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const source = path.join(__dirname, '_redirects');
+const destinationDir  = path.join(__dirname, '../dist/pos-payment/browser/');
+const destination = path.join(destinationDir, '_redirects');
+
+// Ensure the destination directory exists
+if (!fs.existsSync(destinationDir)) {
+    fs.mkdirSync(destinationDir, { recursive: true });
+  }
+
+// Copy the file
+fs.copyFile(source, destination, (err) => {
+  if (err) {
+    console.error('Error copying _redirects file:', err);
+  } else {
+    console.log('_redirects file copied successfully.');
+  }
+});
